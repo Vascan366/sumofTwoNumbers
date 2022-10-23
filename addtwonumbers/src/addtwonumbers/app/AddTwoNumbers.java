c2755752e85fe50dab926425875048de80cdc6cc
vasanth K <vascan366@gmail.com>
2022-10-23 10:01:35 -0500

    initial project creation

---
+++ addtwonumbers/src/addtwonumbers/app/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+package addtwonumbers.app;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b =20, c = 0;
+		System.out.println("sum of a, b : "+  (a+b) );
+
+	}
+
+}
